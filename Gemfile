c90f4e20b01b3be6ab626512ffd651853fc8afd2
Kohl Kohlbrenner <edwin.kohlbrenner@gmail.com>
2015-05-06 09:37:11 -0400

    figaro

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem "mini_magick"
 gem "refile", require: ["refile/rails", "refile/image_processing"]
 gem 'ransack'
 gem "aws-sdk", '< 2'
+gem 'figaro', '~> 1.1.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,9 +1,9 @@
 require "refile/backend/s3"
 
 aws = {
-  access_key_id: Rails.application.secrets.access_key_id,
-  secret_access_key: Rails.application.secrets.secret_access_key,
-  bucket: Rails.application.secrets.bucket
+  access_key_id: ENV["access_key_id"],
+  secret_access_key: ENV["secret_access_key"],
+  bucket: ENV["bucket"]
 }
 Refile.cache = Refile::Backend::S3.new(prefix: "cache", **aws)
 Refile.store = Refile::Backend::S3.new(prefix: "store", **aws)
